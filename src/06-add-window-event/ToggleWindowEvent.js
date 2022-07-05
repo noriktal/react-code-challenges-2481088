2cd8ebb7e132318b979d9156bbf41b91cd6c29e2
Michal Tal-Socher <80818338+noriktal@users.noreply.github.com>
2022-07-05 21:40:08 +0300

    challenge 6 completed

--- src/06-add-window-event/ToggleWindowEvent.js
+++ src/06-add-window-event/ToggleWindowEvent.js
@@ -1,8 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import WindowEvent from './WindowEvent'
 
+const alertMe = () => {alert('you have double clicked me!')};
+
 export default function ToggleWindowEvent () {
-  const [windowEvent, setWindowEvent] = useState(false)
+  const [windowEvent, setWindowEvent] = useState(false);
+
+  useEffect(() => {
+    if(windowEvent){
+      window.addEventListener('dblclick', alertMe);
+    }else{
+      window.removeEventListener('dblclick', alertMe);
+    }
+    return () => {
+     window.removeEventListener('dblclick', alertMe);
+    }
+  }, [windowEvent])
+  
+
+
   return (
     <div>
       <button onClick={() => setWindowEvent(prevState => !prevState)}>Toggle Window Event</button>
