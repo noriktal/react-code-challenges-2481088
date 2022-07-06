345da0b52a797771ec9540b422bc1cb8c5b52a39
Michal Tal-Socher <80818338+noriktal@users.noreply.github.com>
2022-07-06 09:45:45 +0300

    challenge 8 compleyted with useContext + useState

--- src/08-pixel-art/PixelArt.js
+++ src/08-pixel-art/PixelArt.js
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, { useContext, useState } from 'react';
+import { ColorContext } from '../App';
+
 
 function ColorPicker () {
+ 
+  const { setClickedColor } = useContext(ColorContext);
+
   const colors = ['red', 'blue', 'yellow', 'green', 'black', 'white', 'purple']
+  
+  const handleClick = (color) => {
+    setClickedColor(color)
+  };
+  
   return (
     <div>
       <h1>Choose a color</h1>
-      {colors.map(color => <button key={color} style={{ backgroundColor: color }} />)}
+      {colors.map(color => <button key={color} onClick={() => handleClick(color)} style={{ backgroundColor: color }} />)}
     </div>
   )
 }
 
 function Pixel () {
-  return <div style={{ height: '20px', width: '20px', backgroundColor: 'lightGrey', margin: '1px' }} />
-}
+
+  const [myColor, setMyColor] = useState('#fff');
+  const { clickedColor } = useContext(ColorContext);
+  return <div 
+          style={{ height: '20px', width: '20px', backgroundColor: myColor, margin: '1px', border: '1px solid grey' }}
+          onClick={() => setMyColor(clickedColor)}
+          />
+        }
 
 function Pixels () {
   const pixels = []
--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import './App.css'
-import PixelArt from './08-pixel-art/PixelArt'
+import React, { createContext, useState, useMemo } from 'react';
+import './App.css';
+import PixelArt from './08-pixel-art/PixelArt';
+
+export const ColorContext = React.createContext({
+  clickedColor: '',
+  setClickedColor: () => {}
+});
+
 
 function App () {
+
+  const [clickedColor, setClickedColor] = useState('#000');
+   const value = useMemo(() => ({
+    clickedColor, setClickedColor
+  }), [clickedColor]);
+
   return (
-    <div className='App'>
-      <PixelArt />
-    </div>
+    
+    <ColorContext.Provider value={value} >
+      <div className='App'>
+        <PixelArt />
+      </div>
+    </ColorContext.Provider >
   )
 }
 
-export default App
+export default App;
