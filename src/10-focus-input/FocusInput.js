3128f7838d6ab39ac95d1e9fc735ce30e1e73623
Michal Tal-Socher <80818338+noriktal@users.noreply.github.com>
2022-07-06 12:52:46 +0300

    challenge 10- completed

--- src/10-focus-input/FocusInput.js
+++ src/10-focus-input/FocusInput.js
@@ -1,8 +1,19 @@
+
+import { useRef, useEffect } from "react";
+
 export default function FocusInput() {
+
+  const inputRef = useRef(null);
+
+useEffect(() => {
+ inputRef.current.focus();
+}, [])
+
+
   return (
     <div>
       <label htmlFor='focused-input'>Focus me on page load!</label>
-      <input name='focused-input'></input>
+      <input name='focused-input' ref={inputRef}></input>
     </div>
   )
 }
